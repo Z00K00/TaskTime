39eabfd3b3b4ff8fd731ee679cb70e686c629c30
Kraig Larson <147437185+Z00K00@users.noreply.github.com>
2024-03-01 14:22:11 -0500

    Initial Commit

---
+++ TaskTime/TaskTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTimeApp.swift
+//  TaskTime
+//
+//  Created by Krajg Larson on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
